717b389110dc9aaf0a8eb4ba556b234987a7b2be
mcampos05 <mcampos05@gmail.com>
2014-03-17 09:03:55 -0600

    Es el primer proyecto creado en .NET

---
+++ publicador_ml_net/publicador_ml_net/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace publicador_ml_net.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ publicador_ml_net/publicador_ml_net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="publicador_ml_net.WebApiApplication" Language="C#" %>
